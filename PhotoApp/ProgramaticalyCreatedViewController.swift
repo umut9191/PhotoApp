b5caf47cc5e31fa2bb55572b020a86ceb82c8d1f
umut9191 <umutsurmeli91@gmail.com>
2022-03-24 00:18:44 +0300

    storyboard we done before but for how to write unit test XIB ViewContoller and without xib file programaticliy created view controller(creation way like )

---
+++ PhotoApp/ProgramaticalyCreatedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProgramaticalyCreatedViewController.swift
+//  PhotoApp
+//
+//  Created by Macintosh on 24.03.2022.
+//
+
+import UIKit
+
+class ProgramaticalyCreatedViewController: UIViewController {
+//This view controller created as well as XIBBaseViewController the differance when created cocoa touc class to anable created XIB file
+    //We add this code manuelly;
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
